927ef4f6ebc20b30509f7f2fc93dd00a20a615aa
Jefferson Augusto Ribeiro <74573131+jefferson-ribeiro@users.noreply.github.com>
2022-07-07 21:47:38 -0300

    s3bucket, lambda, sns, bucket_notification

--- local_file_exemplos/lambda_terraform/main.tf
+++ local_file_exemplos/lambda_terraform/main.tf
@@ -18,25 +18,6 @@ resource "aws_s3_bucket" "jeff-s3-bucket-origin" {
   }
 }
 
-resource "aws_lambda_function" "lambda_handler" {
-  # If the file is not in the current working directory you will need to include a 
-  # path.module in the filename.
-  filename      = "my-deployment-package.zip"
-  function_name = "lambda_handler"
-  role          = "arn:aws:lambda:us-east-1:215226549206:function:lambda_handler"
-  handler       = "lambda_function.py"
-
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  # source_code_hash = filebase64sha256("lambda_function_payload.zip")
-
-  runtime = "python3.8"
-  layers = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSDataWrangler-Python38:6"]
-}
-
-
-
 resource "aws_s3_bucket" "jeff-s3-bucket-destiny" {
   bucket = "jeff-s3-bucket-destiny"
   tags = {
@@ -44,3 +25,59 @@ resource "aws_s3_bucket" "jeff-s3-bucket-destiny" {
     Environment = "Dev"
   }
 }
+
+resource "aws_lambda_function" "lambda_test" {
+  # If the file is not in the current working directory you will need to include a 
+  # path.module in the filename.
+  filename      = "my-deployment-package.zip"
+  function_name = "lambda_test"
+  role          = "arn:aws:iam::215226549206:role/service-role/lambda_handler-role-bnlhzmtl"
+  handler       = "lambda_function.py"
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  # source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  runtime = "python3.8"
+  layers = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSDataWrangler-Python38:6"]
+}
+
+resource "aws_sns_topic" "s3-event-notification-topic" {
+  name = "s3-event-notification-topic"
+
+  policy = <<POLICY
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Action": "SNS:Publish",
+      "Resource": "arn:aws:sns:us-east-1:215226549206:s3-event-notification-topic",
+      "Condition": {
+        "ArnLike": {
+          "AWS:SourceArn": "arn:aws:s3:::jeff-s3-bucket-origin"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.s3-event-notification-topic.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.lambda_test.arn
+}
+
+
+resource "aws_s3_bucket_notification" "s3-bucket-origin-notification" {
+  bucket = aws_s3_bucket.jeff-s3-bucket-origin.id
+  topic {
+    topic_arn     = aws_sns_topic.s3-event-notification-topic.arn
+    events        = ["s3:ObjectCreated:*"]
+  }
+}
+
