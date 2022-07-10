f5d1b8bee8655441fa528a4bfcb45d2d27d70341
Jefferson Augusto Ribeiro <74573131+jefferson-ribeiro@users.noreply.github.com>
2022-07-10 20:26:27 -0300

    ajuste de configuração de lambda

--- local_file_exemplos/lambda_terraform/main.tf
+++ local_file_exemplos/lambda_terraform/main.tf
@@ -26,19 +26,20 @@ resource "aws_s3_bucket" "jeff-s3-bucket-destiny" {
   }
 }
 
-resource "aws_lambda_function" "lambda_test" {
+resource "aws_lambda_function" "lambda_handler" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
   filename      = "my-deployment-package.zip"
-  function_name = "lambda_test"
+  function_name = "lambda_handler"
   role          = "arn:aws:iam::215226549206:role/service-role/lambda_handler-role-bnlhzmtl"
-  handler       = "lambda_function.py"
+  handler       = "lambda_function.lambda_handler"
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
   # source_code_hash = filebase64sha256("lambda_function_payload.zip")
   runtime = "python3.8"
-  layers = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSDataWrangler-Python38:6"]
+  layers = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSDataWrangler-Python38:8"]
+  timeout = 12
 }
 
 resource "aws_sns_topic" "s3-event-notification-topic" {
@@ -69,15 +70,15 @@ POLICY
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.s3-event-notification-topic.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.lambda_test.arn
+  endpoint  = aws_lambda_function.lambda_handler.arn
 }
 
 resource "aws_lambda_permission" "lambda_authorizer_permission" {
   statement_id  = "AllowExecutionFromSNS"
   action        = "lambda:InvokeFunction"
-  function_name = "lambda_test"
+  function_name = "lambda_handler"
   principal     = "sns.amazonaws.com"
-  source_arn    = "arn:aws:sns:us-east-1:215226549206:s3-event-notification-topic"
+  source_arn    = aws_sns_topic_subscription.user_updates_sqs_target.topic_arn
 }
 
 resource "aws_s3_bucket_notification" "s3-bucket-origin-notification" {
